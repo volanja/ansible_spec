fbbd74c983cc77eff89d388609dbfb51f08290f6
Stefan Horning <stefan@hornings.de>
2015-09-04 10:43:07 +0200

    Improved Rakefile and spec_helper working together in getting the ansible variables defined in a playbook accross to the spec files.

--- lib/src/spec/spec_helper.rb
+++ lib/src/spec/spec_helper.rb
@@ -1,7 +1,15 @@
 require 'serverspec'
 require 'net/ssh'
+require 'yaml'
+require 'json'
 
-set :backend, :ssh
+if ENV['CONNECTION'] == 'local'
+  set :backend, :exec
+#  set :pre_command, 'sudo -s'
+else
+  set :backend, :ssh
+  # automatically runs as sudo
+end
 
 if ENV['ASK_SUDO_PASSWORD']
   begin
@@ -28,9 +36,27 @@ set :ssh_options, options
 # Disable sudo
 # set :disable_sudo, true
 
-
 # Set environment variables
 # set :env, :LANG => 'C', :LC_MESSAGES => 'C'
 
 # Set PATH
 # set :path, '/sbin:/usr/local/sbin:$PATH'
+
+# Getting the ansible variables from included vars_files and
+# playbook vars to be usable in the tests
+vars_files ||= ENV['VARS_FILES']
+ansible_vars ||= ENV['VARS'] ? JSON.load(ENV['VARS']) : {}
+
+if vars_files
+  vars = {}
+  vars_files.split(',').each do |file|
+    tmp_vars = YAML.load_file(file)
+    tmp_vars.each do |k, v|
+      v.gsub!(/{{(.+)}}/, "#{tmp_vars[$1]}")
+    end
+    vars.merge!(tmp_vars)
+  end
+  ansible_vars.merge!(vars)
+end
+
+ANSIBLE_VARS = ansible_vars
